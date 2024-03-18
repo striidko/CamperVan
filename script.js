241a2b304f2e8dc26a3b287a7d080a36868e4c1d
sopogobejishvil <30219805-sopogobejishvil@users.noreply.replit.com>
2024-03-18 09:54:58 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let slideIndex = 0;
+slideshow();
+
+function slideshow() {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(slideshow, 3000);
+}
